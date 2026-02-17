560b6640314a3015bc1d32d05cde082ff83687ab
malhar-paradkar <malharparadkar@gmail.com>
2026-02-18 01:13:10 +0530

    feat(db): implement MongoDB connection logic

---
+++ server/src/config/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connection successful: ${conn.connection.host}`);
+    }   catch (error) {
+        console.log("Database connection failed: ", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
