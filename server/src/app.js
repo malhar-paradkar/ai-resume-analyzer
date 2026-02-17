f9e079a27d1551ded7acb65520f9bdac475e9377
malhar-paradkar <malharparadkar@gmail.com>
2026-02-18 01:21:00 +0530

    feat(app): setup express application with middleware

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const healthRoutes = require ("./routes/health.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/health", healthRoutes);
+
+module.exports = app;
