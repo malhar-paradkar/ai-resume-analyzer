98e13ba9d4275fc9d6bd17d86a2ee97175b8dbe0
malhar-paradkar <malharparadkar@gmail.com>
2026-02-19 20:51:35 +0530

    feat(auth): add authentication routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes";
 
 const healthRoutes = require ("./routes/health.routes");
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
